87197d8eae68fc684569c09b4feb7b48afabcd38
lohithkola06 <lohithkola06@gmail.com>
2026-03-23 18:00:45 +0530

    Error 14: include owned properties in player net worth

--- whitebox/code/moneypoly/moneypoly/player.py
+++ whitebox/code/moneypoly/moneypoly/player.py
@@ -35,7 +35,11 @@ class Player:  # pylint: disable=too-many-instance-attributes
 
     def net_worth(self):
         """Calculate and return this player's total net worth."""
-        return self.balance
+        property_value = sum(
+            prop.mortgage_value if prop.is_mortgaged else prop.price
+            for prop in self.properties
+        )
+        return self.balance + property_value
 
     def move(self, steps):
         """
