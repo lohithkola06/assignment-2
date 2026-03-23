d97ff9671a735f91f9f251260ea87abdb2fe5bc6
lohithkola06 <lohithkola06@gmail.com>
2026-03-23 18:00:03 +0530

    Error 13: award go salary when players pass the start square

--- whitebox/code/moneypoly/moneypoly/player.py
+++ whitebox/code/moneypoly/moneypoly/player.py
@@ -43,11 +43,12 @@ class Player:  # pylint: disable=too-many-instance-attributes
         Awards the Go salary if the player passes or lands on Go.
         Returns the new board position.
         """
+        old_position = self.position
         self.position = (self.position + steps) % BOARD_SIZE
 
-        if self.position == 0:
+        if steps > 0 and self.position <= old_position:
             self.add_money(GO_SALARY)
-            print(f"  {self.name} landed on Go and collected ${GO_SALARY}.")
+            print(f"  {self.name} passed Go and collected ${GO_SALARY}.")
 
         return self.position
 
